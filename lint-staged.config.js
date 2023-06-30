3f574c3013e3138997dd6d1194aa083cb131b1c1
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2023-06-30 19:26:14 +0800

    chore: update pre commit and pre push hooks lint and test commands

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  'packages/**/*.{ts,tsx}': (files) => `nx affected -t type-check --files=${files.join(',')}`,
+  'packages/**/*.{ts,tsx}': (files) =>
+    `nx affected -t lint:type --files=${files.join(',')}`,
   'packages/**/*.{js,ts,jsx,tsx,json}': (files) =>
     `nx affected:lint --fix --files=${files.join(',')}`,
-  '**/*.{js,ts,jsx,tsx,json,cjs,html,css}': (files) => `nx format:write --files=${files.join(',')}`,
+  '**/*.{js,ts,jsx,tsx,json,cjs,html,css}': (files) =>
+    `nx format:write --files=${files.join(',')}`,
 };
