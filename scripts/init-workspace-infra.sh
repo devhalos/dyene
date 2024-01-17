7f7880a676e4c1af644a93550fcb1a02f282cc4c
Jayson Ojeda <jayson.unto.ojeda@gmail.com>
2024-01-17 21:14:20 +0800

    chore(web-app): add infra debugging

--- scripts/init-workspace-infra.sh
+++ scripts/init-workspace-infra.sh
@@ -6,3 +6,4 @@ cd infrastructure || exit 1;
 
 echo "Switching to workspace $WORKSPACE in $PWD"
 terraform workspace select "$WORKSPACE" || terraform workspace new "$WORKSPACE"
+echo "Done switching workspace"
